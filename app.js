7b2641c713c8a3428956b114955fe0c7e2403e11
Anthony Garvan <anthony.garvan@gsa.gov>
2016-03-05 15:32:58 -0600

    pretty much working

--- app.js
+++ app.js
@@ -3,8 +3,60 @@ var request = require('request');
 
 var token = process.env.SLACK_API_TOKEN || '';
 
+function openGroupChat(coffeemateId, coffeeQueue, rtm) {
+	var users = coffeemateId + ',' + coffeeQueue[0] + ',' + coffeeQueue[1];
+	var options = {url: "https://slack.com/api/mpim.open",
+					qs: {token: token, users: users}};
+	request.get(options, function(err, response, body) {
+	    bodyObj = JSON.parse(body);
+	    if(bodyObj.ok) {
+	    	rtm.sendMessage("Hey! You two have been paired up for coffee. Next step is to figure out a time that works for both of you. Enjoy! :coffee:"
+	  			, bodyObj.group.id);
+	    }
+});
+}
 
-function getCoffeemateId(callback) {
+var coffeeQueue = []
+function listenForPrompts(coffeemateId) {
+	var rtm = new RtmClient(token, {logLevel: 'debug'});
+	rtm.start();
+
+	var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+	rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+	  if(message.type === 'message' && message.text.toLowerCase().indexOf('coffee me') >= 0) {
+	  	coffeeQueue.push(message.user);
+	  	if(coffeeQueue.length === 1) {
+	  		rtm.sendMessage("<@" + message.user + ">: You're in line for coffee!" +
+	  			" You'll be introduced once the next person signs up.", message.channel);	
+	  	}
+
+	  	if(coffeeQueue.length === 2) {
+	  		if(coffeeQueue[0] !== coffeeQueue[1]) {
+		  		rtm.sendMessage("<@" + message.user + ">: " + 
+		  			"You've been matched up for coffee with <@" + coffeeQueue[0] + ">!" + 
+		  			" I'll set up a direct message channel for you two. :coffee: :fireworks:", 
+		  			message.channel);	
+		  		openGroupChat(coffeemateId, coffeeQueue, rtm);	  			
+	  		} else {
+	  			rtm.sendMessage("<@" + message.user + ">: " + 
+	  				"You were just paired with yourself for coffee! You really don't need a " +
+	  				"bot's orchestration for this, you can just go get a cup. You are no longer " +
+	  				"in line to get coffee.", 
+		  			message.channel);
+	  		}
+
+	  		coffeeQueue = [];
+	  	}
+	  }
+
+	  if(message.type === 'message' && message.text.indexOf('coffee queue') >= 0) {
+	  		rtm.sendMessage("<@" + message.user + ">: There are " + coffeeQueue.length + " folks in line for coffee."
+	  			, message.channel);	
+	   };	
+});}
+
+function run() {
 	var options = {url: "https://slack.com/api/users.list",
 					qs: {token: token}};
 	request.get(options, function(err, response, body) {
@@ -12,30 +64,8 @@ function getCoffeemateId(callback) {
 	    if(bodyObj.ok) {
 	    	var coffeemateId = bodyObj.members.filter(function(member) {
 	    		return member.name === 'coffeemate'})[0].id
-	    	console.log(coffeemateId);
-	    	callback(coffeemateId);
+	    	listenForPrompts(coffeemateId);
 	    }
 })}
 
-
-
-	
-function listenForPrompts() {
-	var rtm = new RtmClient(token, {logLevel: 'debug'});
-	rtm.start();
-
-	var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-
-	rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-		console.log('-------MESSAGE---------');
-		console.log(message);
-	  if(message.type === 'message' && message.text.indexOf('coffee me') >= 0) {
-	  	console.log('YOU HAVE SIGNED UP FOR VIRTUAL COFFEE!!!!!')
-	  	console.log(message);
-
-	  	rtm.sendMessage("<@" + message.user + ">: You're signed up for coffee!", message.channel);	
-	  }
-	});	
-}
-
-getCoffeemateId(listenForPrompts)
+run();
