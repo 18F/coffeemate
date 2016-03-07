a37d6bc42cc257d294f45632939529ef051bd965
Anthony Garvan <anthony.garvan@gsa.gov>
2016-03-07 10:20:29 -0600

    adding tests

---
+++ bot.js
@@ -0,0 +1,79 @@
+function Bot(rtm, request, token) {
+	var coffeeQueue = []
+	function openGroupChat(coffeemateId, coffeeQueue) {
+		var users = coffeemateId + ',' + coffeeQueue[0] + ',' + coffeeQueue[1];
+		var options = {url: "https://slack.com/api/mpim.open",
+						qs: {token: token, users: users}};
+		request.get(options, function(err, response, body) {
+		    bodyObj = JSON.parse(body);
+		    if(bodyObj.ok) {
+		    	rtm.sendMessage("Hey! You two have been paired up for coffee. Next step is to figure out a time that works for both of you. Enjoy! :coffee:"
+		  			, bodyObj.group.id);
+		    }
+	});
+	}
+
+	function listenForPrompts(coffeemateId) {
+		rtm.start();
+
+		var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+		rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+		  if(message.type === 'message' && message.text.toLowerCase().indexOf('coffee me') >= 0
+		  		&& message.user !== coffeemateId) {
+		  	coffeeQueue.push(message.user);
+		  	if(coffeeQueue.length === 1) {
+		  		rtm.sendMessage("<@" + message.user + ">: You're in line for coffee!" +
+		  			" You'll be introduced to the next person who wants coffee.", message.channel);	
+		  	}
+
+		  	if(coffeeQueue.length === 2) {
+		  		if(coffeeQueue[0] !== coffeeQueue[1]) {
+			  		rtm.sendMessage("<@" + message.user + ">: " + 
+			  			"You've been matched up for coffee with <@" + coffeeQueue[0] + ">!" + 
+			  			" I'll set up a direct message channel for you two. :coffee: :fireworks:", 
+			  			message.channel);	
+			  		openGroupChat(coffeemateId, coffeeQueue, rtm);	  		
+		  		} else {
+		  			rtm.sendMessage("<@" + message.user + ">: " + 
+		  				"You just paired with yourself for coffee! You really don't need a " +
+		  				"bot to orchestrate this, you can just go get a cup. You are no longer " +
+		  				"in line to get coffee.", 
+			  			message.channel);
+		  		}
+
+		  		coffeeQueue = [];
+		  	}
+		  } else {
+		  	if(message.type === 'message' && 
+		  		(message.text.indexOf(coffeemateId) >= 0 || message.channel[0] == 'D')) {
+		  		rtm.sendMessage("I'm just a simple, creamy, matchmaking robot. To set up a virtual coffee, " + 
+		  				"direct message me and say `coffee me`, and I'll match you with another " +
+		  				"18fer who has done the same thing. You can also post `coffee me` in any channel " +
+		  				"I am invited to.", 
+			  			message.channel);
+		  	}
+		  }
+
+		  if(message.type === 'message' && message.text.indexOf('coffee queue') >= 0) {
+		  		rtm.sendMessage("<@" + message.user + ">: There are " + coffeeQueue.length + " folks in line for coffee."
+		  			, message.channel);	
+		   };	
+	});}
+
+	function run() {
+		var options = {url: "https://slack.com/api/users.list",
+						qs: {token: token}};
+		request.get(options, function(err, response, body) {
+		    bodyObj = JSON.parse(body);
+		    if(bodyObj.ok) {
+		    	var coffeemateId = bodyObj.members.filter(function(member) {
+		    		return member.name === 'coffeemate'})[0].id
+		    	listenForPrompts(coffeemateId);
+		    }
+	})}
+
+	return {run: run};
+}
+
+module.exports.Bot = Bot;
---
+++ spec/botSpec.js
@@ -0,0 +1,113 @@
+bot = require('../bot.js');
+describe("coffeebot works", function() {
+	var request;
+	var rtm;
+	var token;
+	var messages;
+	var channels;
+
+	beforeEach(function() {
+		token ='testToken';
+		request= {get: function(options, callback) {
+			if(options.url === 'https://slack.com/api/users.list') {
+				callback(null, null, JSON.stringify({ok: true, members: [{name: 'coffeemate', id: 'coffemateId1234'}]}));
+			};
+
+			if(options.url === 'https://slack.com/api/mpim.open') {
+				callback(null, null, JSON.stringify({ok: true, group: {id: 'groupId1234'}}));
+			};
+		}};
+		messages = [];
+		channels = [];
+		rtm = {start: function(){}, 
+				   sendMessage: function(message, channel) {
+				   		messages.push(message);
+				   		channels.push(channel);
+				  	}}
+	}) 
+	
+	it("responds to coffee me", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", text: "coffee me", user: "userA", channel: "ch1"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		expect(messages[0].indexOf('in line') > 0).toBe(true);
+		expect(channels[0]).toBe('ch1');
+		done();
+	});
+
+	it("responds to coffee queue", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", text: "coffee queue", user: "userA", channel: "ch1"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		expect(messages[0].indexOf('0 folks') > 0).toBe(true);
+		expect(channels[0]).toBe('ch1');
+		done();
+	});
+	
+	it("responds with help text when mentioned", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", 
+							text: "<@coffemateId1234>: blahblah", user: "userA", channel: "ch1"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		expect(messages[0].indexOf("I'm just a simple") >= 0).toBe(true);
+		expect(channels[0]).toBe('ch1');
+		done();
+	});
+
+	it("responds with help text when direct messaged", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", 
+							text: "blahblah", user: "userA", channel: "D1234"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		expect(messages[0].indexOf("I'm just a simple") >= 0).toBe(true);
+		expect(channels[0]).toBe('D1234');
+		done();
+	});
+
+	it("pairs up users for coffee, sending message and setting up group chat", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", 
+							text: "coffee me", user: "userA", channel: "ch1"});
+						callback({type: "message", 
+							text: "please Coffee Me!", user: "userB", channel: "ch2"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		expect(messages[0].indexOf('in line') > 0).toBe(true);
+		expect(channels[0]).toBe('ch1');
+		expect(messages[1].indexOf("matched up for coffee") >= 0).toBe(true);
+		expect(channels[1]).toBe('ch2');
+		expect(messages[2].indexOf("You two have been paired up") >= 0).toBe(true);
+		expect(channels[2]).toBe('groupId1234');
+		done();
+	});
+
+	it("empties queue when self pairing", function(done) {
+		rtm.on = function(event, callback) {
+						callback({type: "message", 
+							text: "coffee me", user: "userA", channel: "ch1"});
+						callback({type: "message", 
+							text: "please Coffee Me!", user: "userA", channel: "ch1"});
+						callback({type: "message", 
+							text: "coffee queue", user: "userA", channel: "ch1"});
+						}
+		var coffeemate = bot.Bot(rtm, request, token)
+		coffeemate.run();
+		console.log(messages);
+		expect(messages[0].indexOf('in line') > 0).toBe(true);
+		expect(channels[0]).toBe('ch1');
+		expect(messages[1].indexOf('paired with yourself') >= 0).toBe(true);
+		expect(channels[1]).toBe('ch1');
+		expect(messages[2].indexOf('0 folks') >= 0).toBe(true);
+		expect(channels[2]).toBe('ch1');
+		done();
+	});
+});
